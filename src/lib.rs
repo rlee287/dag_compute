15b02b119953df6362710bcd2a71dc2d8ad53734
rlee287 <rlee287@yahoo.com>
2021-11-23 21:45:53 -0500

    Implement base refcount for cleanup during computation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 
-use slotmap::{SlotMap, new_key_type};
+use slotmap::{SlotMap, SecondaryMap, new_key_type};
 
 use std::collections::{HashSet, VecDeque};
 
@@ -54,6 +54,7 @@ pub struct NodeHandle {
 
 pub struct ComputationGraph<T> {
     node_storage: SlotMap<ComputeGraphKey, Node<T>>,
+    node_refcount: SecondaryMap<ComputeGraphKey, u32>,
     //node_refcount:
     output_node: Option<ComputeGraphKey>,
     graph_id: usize
@@ -62,6 +63,7 @@ impl<T> Default for ComputationGraph<T> {
     fn default() -> Self {
         let mut obj = ComputationGraph {
             node_storage: SlotMap::default(),
+            node_refcount: SecondaryMap::default(),
             output_node: None,
             graph_id: 0
         };
@@ -76,6 +78,7 @@ impl<T: Clone> ComputationGraph<T> {
     pub fn insert_node(&mut self, name: String, func: BoxedEvalFn<T>) -> NodeHandle {
         let node = Node::new(name, func);
         let node_key = self.node_storage.insert(node);
+        self.node_refcount.insert(node_key, 0);
         NodeHandle {
             node_key,
             graph_id: self.graph_id
@@ -93,6 +96,7 @@ impl<T: Clone> ComputationGraph<T> {
         let node_key = node.node_key;
         assert!(self.node_storage.contains_key(node_key));
         self.output_node = Some(node_key);
+        *self.node_refcount.get_mut(node_key).unwrap() += 1;
     }
     pub fn set_inputs(&mut self, node: &mut NodeHandle, inputs: &[&NodeHandle]) {
         assert_eq!(node.graph_id, self.graph_id,
@@ -100,6 +104,10 @@ impl<T: Clone> ComputationGraph<T> {
         let input_keys: Vec<_> = inputs.iter().map(|handle| handle.node_key).collect();
         assert!(!input_keys.contains(&node.node_key), "Inputs would create self-loop");
         // Other cycles would be caught at computation time
+
+        for key in input_keys.iter() {
+            *self.node_refcount.get_mut(*key).unwrap() += 1;
+        }
         self.node_storage.get_mut(node.node_key).unwrap().input_nodes = input_keys;
     }
 
@@ -145,16 +153,31 @@ impl<T: Clone> ComputationGraph<T> {
         for node_key in self.computation_order() {
             let node = self.node_storage.get(node_key).unwrap();
             println!("Evaluating {}", node.name);
+            println!("{:?}", self.node_refcount);
 
             let node_input_keyvec = node.input_nodes.clone();
+            let mut nodes_cleanup = Vec::with_capacity(node_input_keyvec.len());
             let node_inputs: Vec<_> = node_input_keyvec.into_iter().map(|key| {
+                let in_refcnt = self.node_refcount.get_mut(key).unwrap();
+                assert!(*in_refcnt > 0);
+                *in_refcnt -= 1;
+                if *in_refcnt == 0 {
+                    nodes_cleanup.push(key);
+                }
                 self.node_storage.get(key).unwrap().computed_val()
             }).collect();
+
+            for old_key in nodes_cleanup {
+                self.node_storage.remove(old_key);
+                self.node_refcount.remove(old_key);
+            }
             // Rebind node as &mut to perform calculation
             let node = self.node_storage.get_mut(node_key).unwrap();
             // Toposort guarantees that inputs will be ready when needed
+            println!("{:?}", self.node_refcount);
             node.eval(node_inputs.as_slice());
         }
+        assert_eq!(self.node_storage.len(), 1);
         self.node_storage.get(self.output_node.take().unwrap()).unwrap().computed_val()
     }
 }
